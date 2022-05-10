7d28321350bf9eee7b829cf9bbfd49a184ce15c8
Siddhesh Sarfare <105264257+Siddhesh-Sarfare@users.noreply.github.com>
2022-05-10 13:56:09 +0530

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/
+
+define('DB_USER', "id18543900_lalightning");     // Your database user name
+define('DB_PASSWORD', "Varshavivek@05");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id18543900_lightning"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
